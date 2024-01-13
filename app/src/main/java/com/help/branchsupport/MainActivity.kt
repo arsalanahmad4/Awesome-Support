5aacad599fc8e56479ac957ce9659a2a5a623bcb
Arsalan <arsalanahmad0407@gmail.com>
2024-01-14 03:53:39 +0530

    gettingPackageName

--- app/src/main/java/com/help/branchsupport/MainActivity.kt
+++ app/src/main/java/com/help/branchsupport/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.help.branchsupport
 
 import android.content.Context
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +19,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.help.branchsupport.ui.theme.BranchSupportTheme
+import java.security.MessageDigest
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +36,9 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-            getInstalledPackageNames(this)
+            //getInstalledPackageNames(this)
+            getallapps(this)
+            printSampleSha1List(this)
         }
     }
 
@@ -47,6 +56,112 @@ class MainActivity : ComponentActivity() {
         }
 
         return packageNames
+    }
+
+    fun getallapps() {
+        // get list of all the apps installed
+        val infos = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+        // create a list with size of total number of apps
+        val apps = arrayOfNulls<String>(infos.size)
+        var i = 0
+        // add all the app name in string list
+        for (info in infos) {
+            apps[i] = info.packageName
+            Log.e("Package Name", " -> " + apps[i])
+            i++
+        }
+        // set all the apps name in list view
+        Toast.makeText(this, infos.size.toString() + " Apps are installed", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    // https://tomas-repcik.medium.com/listing-all-installed-apps-in-android-13-via-packagemanager-3b04771dc73
+    fun getallapps(context: Context) {
+        val pm = context.packageManager
+        val mainIntent = Intent(Intent.ACTION_MAIN, null)
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        val resolvedInfos = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.queryIntentActivities(
+                mainIntent,
+                PackageManager.ResolveInfoFlags.of(0L)
+            )
+        } else {
+            pm.queryIntentActivities(mainIntent, 0)
+        }
+    }
+
+    //https://stackoverflow.com/questions/46390522/android-get-list-of-installed-apps-signatures
+    fun printSampleSha1List(ctx: Context) {
+        val packages: List<ApplicationInfo> =
+            ctx.packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+
+        for (pkg in packages) {
+            var packageInfo: PackageInfo? = null
+            try {
+                packageInfo = ctx.packageManager.getPackageInfo(
+                    pkg.packageName,
+                    PackageManager.GET_SIGNATURES
+                )
+            } catch (e: PackageManager.NameNotFoundException) {
+                e.printStackTrace()
+            }
+
+            packageInfo?.signatures?.forEach { signature ->
+                // SHA1 the signature
+                val sha1 = getSHA1(signature.toByteArray())
+
+                Log.i("Sha1", "name:${pkg.packageName}, $sha1")
+                // Note: sample just checks the first signature
+                return@forEach
+            }
+        }
+    }
+
+    fun getSHA1(sig: ByteArray): String {
+        val digest: MessageDigest
+        try {
+            digest = MessageDigest.getInstance("SHA1")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw RuntimeException(e)
+        }
+        digest.update(sig)
+        val hashtext: ByteArray = digest.digest()
+        return bytesToHex(hashtext)
+    }
+
+    // Util method to convert byte array to hex string
+    fun bytesToHex(bytes: ByteArray): String {
+        val hexArray = charArrayOf(
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            'A',
+            'B',
+            'C',
+            'D',
+            'E',
+            'F'
+        )
+        val hexChars = CharArray(bytes.size * 2)
+        for (j in bytes.indices) {
+            val v = bytes[j].toInt() and 0xFF
+            hexChars[j * 2] = hexArray[v ushr 4]
+            hexChars[j * 2 + 1] = hexArray[v and 0x0F]
+        }
+        return String(hexChars)
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }
 
